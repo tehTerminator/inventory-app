21a910e778907a8220c6cad15f139144dc4ddfd8
Prateek Kher <prateek.kher@gmail.com>
2024-04-14 22:07:33 +0530

    Added Default Location

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Location;
+
+class LocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Location::create([
+            'title' => 'Shop'
+        ]);
+    }
+}
