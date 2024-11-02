ad7e48ed2edfc54c42fbf263fe903b43b6fe5fec
Prateek Kher <prateek.kher@gmail.com>
2024-11-03 00:13:25 +0530

    Fixed Type use Product instead of full Location

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Product;
+use App\Models\Product;
 
 class ProductsController extends Controller
 {
