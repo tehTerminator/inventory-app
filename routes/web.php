94071bbdf691678c05b45111efdc2ddeaaf3a5db
Prateek Kher <prateek.kher@gmail.com>
2024-11-02 22:42:40 +0530

    Added Routes for Product and Location

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('/get/authenticated', ['uses' => 'UsersController@authenticate']);
+
+$router->group(['prefix' => 'get', 'middleware' => 'auth'], function () use ($router) {
+    $router->get('products', ['uses' => 'ProductController@fetch']);
+    $router->get('locations', ['uses' => 'LocationController@fetch']);
+});
+
+$router->group(['prefix' => 'create', 'middleware' => 'auth'], function() use ($router) {
+    $router->post('products', ['uses' => 'ProductController@create']);
+    $router->post('locations', ['uses' => 'LocationController@create']);
+});
+
+$router->group(['prefix' => 'update', 'middleware' => 'auth'], function() use ($router) {
+    $router->put('products', ['uses' => 'ProductController@update']);
+    $router->put('locations', ['uses' => 'LocationController@update']);
+});
+
+$router->group(['prefix' => 'delete', 'middleware' => 'auth'], function() use ($router) {
+    $router->delete('products', ['uses' => 'ProductController@delete']);
+    $router->delete('locations', ['uses' => 'LocationController@delete']);
+});
