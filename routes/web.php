6a4af4d2eabc8311cf8de67da5126896d4b9e416
Prateek Kher <prateek.kher@gmail.com>
2025-01-05 12:04:06 +0530

    Added Route for Products and Location

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
-|
+|   
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
@@ -20,8 +20,10 @@ $router->get('/', function () use ($router) {
 $router->get('/get/authenticated', ['uses' => 'UsersController@authenticate']);
 
 $router->group(['prefix' => 'get', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('products', ['uses' => 'ProductController@fetch']);
-    $router->get('locations', ['uses' => 'LocationController@fetch']);
+    $router->get('products', ['uses' => 'ProductsController@fetch']);
+    $router->get('product', ['uses' => 'ProductsController@fetchOne']);
+    $router->get('locations', ['uses' => 'LocationsController@fetchAll']);
+    $router->get('location', ['uses' => 'LocationsController@fetchOne']);
     $router->get('orders/open', ['uses' => 'OrderController@fetchOpen']);
     $router->get('orders/completed', ['uses' => 'OrderController@fetchCompleted']);
     $router->get('customers', ['uses' => 'CustomerController@fetch']);
@@ -29,21 +31,21 @@ $router->group(['prefix' => 'get', 'middleware' => 'auth'], function () use ($ro
 });
 
 $router->group(['prefix' => 'create', 'middleware' => 'auth'], function() use ($router) {
-    $router->post('products', ['uses' => 'ProductController@create']);
-    $router->post('locations', ['uses' => 'LocationController@create']);
-    $router->post('product/image', ['uses' => 'ProductController@uploadImage']);
+    $router->post('product', ['uses' => 'ProductsController@create']);
+    $router->post('location', ['uses' => 'LocationsController@create']);
+    $router->post('product/image', ['uses' => 'ProductsController@uploadImage']);
     $router->post('order', ['uses' => 'OrderController@create']);
     $router->post('customer', ['uses' => 'CustomerController@create']);
 });
 
 $router->group(['prefix' => 'update', 'middleware' => 'auth'], function() use ($router) {
-    $router->put('product', ['uses' => 'ProductController@update']);
+    $router->put('product', ['uses' => 'ProductsController@update']);
     $router->put('location', ['uses' => 'LocationController@update']);
     $router->put('customer', ['uses' => 'CustomerController@update']);
     $router->put('order/update/status', ['uses' => 'OrderController@updateStatus']);
 });
 
 $router->group(['prefix' => 'delete', 'middleware' => 'auth'], function() use ($router) {
-    $router->delete('products', ['uses' => 'ProductController@delete']);
+    $router->delete('products', ['uses' => 'ProductsController@delete']);
     $router->delete('locations', ['uses' => 'LocationController@delete']);
 });
