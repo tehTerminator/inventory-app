6493f607e1369994a9ac9d88d2020f2fa2aa69fa
Prateek Kher <prateek.kher@gmail.com>
2025-01-05 11:58:27 +0530

    Created LocationsController

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -1,24 +1,38 @@
-<?php 
+<?php
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Location;
 
 class LocationsController extends Controller {
-    public function __construct() { }
-
-    public function create(Request $request) {
-        $this->validate($request, [
-            'title' => ['required', 'unique:locations,title', 'string']
-        ]);
-
-        $location = Location::create(['title' => $request->title]);
-        return response()->json($location);
+    public function __construct() {
     }
 
-    public function delete(Request $request) {
-        $this->validate($request, [
-            'id' => ['required', 'exists:locations,id']
-        ]);
+    public function fetchAll() {
+        return response()->json( Location::all() );
+    }
+
+    public function fetchOne( Request $request ) {
+        $this->validate( $request, [
+            'id' => [ 'required', 'numeric', 'exists:locations,id' ]
+        ] );
+
+        return response()->json( Location::find( $request->id ) );
+    }
+
+    public function create( Request $request ) {
+        $this->validate( $request, [
+            'title' => [ 'required', 'unique:locations,title', 'string' ]
+        ] );
+
+        $location = Location::create( [ 'title' => $request->title ] );
+        return response()->json( $location );
+    }
+
+    public function delete( Request $request ) {
+        $this->validate( $request, [
+            'id' => [ 'required', 'exists:locations,id' ]
+        ] );
     }
 }
